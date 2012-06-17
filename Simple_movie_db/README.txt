6447c89066ba1131278a3e56d436809965da99c6
Dave O'Connor <oconnorwork@gmail.com>
2012-06-16 20:55:07 -0400

    initial commit

---
+++ Simple_movie_db/README.txt
@@ -0,0 +1,5 @@
+Simple Sqlite3 stuff. Created my first .db and .sql. 
+
+I'm using Windows 7 at the moment and I have found Cygwin to be invaluable. 
+
+Following along at: http://zetcode.com/databases/sqlitetutorial/
---
+++ Simple_movie_db/cars.sql
@@ -0,0 +1,32 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE Cars(Id integer PRIMARY KEY, Name text, Cost integer);
+INSERT INTO "Cars" VALUES(1,'Audi',52642);
+INSERT INTO "Cars" VALUES(2,'Mercedes',57127);
+INSERT INTO "Cars" VALUES(3,'Skoda',9000);
+INSERT INTO "Cars" VALUES(4,'Volvo',29000);
+INSERT INTO "Cars" VALUES(5,'Bentley',350000);
+INSERT INTO "Cars" VALUES(6,'Citroen',21000);
+INSERT INTO "Cars" VALUES(7,'Hummer',41400);
+INSERT INTO "Cars" VALUES(8,'Volkswagen',21600);
+COMMIT;
+Id                      Name                Cost      
+----------------------  ------------------  ----------
+1                       Audi                52642     
+Id                      Name                Cost      
+----------------------  ------------------  ----------
+1                       Audi                52642     
+Id                      Name                Cost      
+----------------------  ------------------  ----------
+1                       Audi                52642     
+2                       Mercedes            57127     
+3                       Skoda               9000      
+4                       Volvo               29000     
+5                       Bentley             350000    
+6                       Citroen             21000     
+7                       Hummer              41400     
+8                       Volkswagen          21600     
+CREATE TABLE Cars(Id integer PRIMARY KEY, Name text, Cost integer);
+Id                      Name                Cost      
+----------------------  ------------------  ----------
+1                       Audi                52642     
